6c6558e4042fbec88cd8489cc5749d1649649c77
Gessy90 <42389173+Gessy90@users.noreply.github.com>
2020-12-04 14:37:26 +0000

    Learning how to use Dimensions and Detecting Orientation Changes

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, Image, View, Platform, SafeAreaView} from 'react-native';
+import {useDimensions, useDeviceOrientation} from '@react-native-community/hooks'
 
 export default function App() {
+  //to get the dimensions of the screen
+// console.log(Dimensions.get("screen"))
+
+//object destructuring and pick the landscape property from the useDeviceOrientation object
+const {landscape} = useDeviceOrientation();
 
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={{
+        backgroundColor: 'dodgerblue',
+        width: "100%",
+        height: landscape ? "100%" : "30%",
+      }} ></View>
+    </SafeAreaView>
   );
 }
 
