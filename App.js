049db018e7215e9ca09803a9295e8a48851ee3a1
Gessy90 <42389173+Gessy90@users.noreply.github.com>
2020-12-04 15:02:04 +0000

    Flexbox - Direction, justifyContent , alignItems, alignSelf ,
    - justifyContent is used for the main/primary axis;
    - alignItems is used for the secondary axis;

--- App.js
+++ App.js
@@ -3,32 +3,40 @@ import {View} from 'react-native';
 
 
 export default function App() {
+  //the views are laid out VERTICALLY in a column, because that makes more sense in a mobile app. Usually we use our mobile in portrait mode
   return (
   <View
     style = {{
       backgroundColor:"white",
       flex: 1,
+      flexDirection: "row", //horizontal axis
+      justifyContent: "center", //main/primary axis
+      alignItems: "center" //secondary axis
     }}
   >
 
     <View
       style = {{
-      backgroundColor:"dodgerblue",
-      flex: 2,
+        backgroundColor:"dodgerblue",
+        width: 100,
+        height: 300,
+        alignSelf: "flex-start"
       }}
     />
 
     <View
       style = {{
         backgroundColor:"gold",
-        flex: 1,
+        width: 100,
+        height: 200,
       }}
     />
 
     <View
       style = {{
         backgroundColor:"tomato",
-        flex: 1,
+        width: 100,
+        height: 100,
       }}
     />
   </View>
