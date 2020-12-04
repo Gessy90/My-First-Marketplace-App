d063bb8fb1d71d734284c2a87bdcbac350a47e93
Gessy90 <42389173+Gessy90@users.noreply.github.com>
2020-12-04 14:50:27 +0000

    Trying Flexbox

--- App.js
+++ App.js
@@ -1,31 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Dimensions, StyleSheet, Text, Image, View, Platform, SafeAreaView} from 'react-native';
-import {useDimensions, useDeviceOrientation} from '@react-native-community/hooks'
+import {View} from 'react-native';
+
 
 export default function App() {
-  //to get the dimensions of the screen
-// console.log(Dimensions.get("screen"))
-
-//object destructuring and pick the landscape property from the useDeviceOrientation object
-const {landscape} = useDeviceOrientation();
-
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={{
-        backgroundColor: 'dodgerblue',
-        width: "100%",
-        height: landscape ? "100%" : "30%",
-      }} ></View>
-    </SafeAreaView>
+  <View
+    style = {{
+      backgroundColor:"white",
+      flex: 1,
+    }}
+  >
+
+    <View
+      style = {{
+      backgroundColor:"dodgerblue",
+      flex: 2,
+      }}
+    />
+
+    <View
+      style = {{
+        backgroundColor:"gold",
+        flex: 1,
+      }}
+    />
+
+    <View
+      style = {{
+        backgroundColor:"tomato",
+        flex: 1,
+      }}
+    />
+  </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
