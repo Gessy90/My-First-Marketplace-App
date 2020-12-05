ec1134a76409f9f892cb690dd817f12c0c56a3df
Gessy90 <42389173+Gessy90@users.noreply.github.com>
2020-12-05 15:06:22 +0000

    ViewImageScreen is now complete

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {View} from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 
 
 export default function App() {
-  return <WelcomeScreen />;
+  return <ViewImageScreen />;
 }
 
 
