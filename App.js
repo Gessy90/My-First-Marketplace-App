b74230469f1981a4aa8741d8dfe4f05bb04be18e
Gessy90 <42389173+Gessy90@users.noreply.github.com>
2020-12-10 16:16:56 +0000

    Building the Icon Component

--- App.js
+++ App.js
@@ -6,13 +6,20 @@ import AppText from "./app/components/AppText";
 import {MaterialCommunityIcons} from '@expo/vector-icons';
 import AppButton from './app/components/AppButton';
 import Card from "./app/components/Card";
+import Screen from "./app/components/Screen";
+import Icon from "./app/components/Icon";
 import colors from './app/config/colors';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 import MessagesScreen from './app/screens/MessagesScreen';
 
 
 export default function App() {
-return < MessagesScreen />;
+return < Screen>
+  <Icon
+    name="email"
+    size={100}
+  />
+</Screen>
 }
 
 
